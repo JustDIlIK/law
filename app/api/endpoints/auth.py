47a095e93bfc5597742c6c6caf5532ce7b181dcc
JustDIlIK <dilavar7517@gmail.com>
2025-09-26 12:48:10 +0500

    Adding GET

--- app/api/endpoints/auth.py
+++ app/api/endpoints/auth.py
@@ -82,6 +82,6 @@ async def login_user(response: Response):
     response.delete_cookie("token")
 
 
-@router.post("/current-user/", response_model=SUsersGetCurrent)
+@router.get("/current-user/", response_model=SUsersGetCurrent)
 async def login_user(user=Depends(get_current_user)):
     return user
