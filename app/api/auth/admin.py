6c8b42e548bb2240b349c83e869653a577e909f6
JustDIlIK <dilavar7517@gmail.com>
2025-10-17 13:20:22 +0500

    S6

--- app/api/auth/admin.py
+++ app/api/auth/admin.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 from fastapi import Request, HTTPException, Depends
 from jose import jwt, JWTError
-from starlette.responses import JSONResponse
+from starlette.responses import JSONResponse, RedirectResponse
 
 from app.config.config import settings
 from app.db.repository.admin import AdminRepository
@@ -18,19 +18,20 @@ def get_token(request: Request):
 
 async def get_current_user(token: str):
     try:
-        print("Here")
         payload = jwt.decode(token, settings.KEY, algorithms=[settings.ALGORITHM])
     except JWTError:
-        raise HTTPException(status_code=401, detail="Неверный токен")
-    print("Here")
+        return RedirectResponse(url="/", status_code=302)
+
     exp = payload.get("exp")
     if not exp or int(exp) < datetime.utcnow().timestamp():
-        raise HTTPException(status_code=401, detail="Токен истёк")
+        return RedirectResponse(url="/", status_code=302)
 
     user_id = payload.get("sub")
     if not user_id:
-        raise HTTPException(status_code=401, detail="Неверный токен")
+        return RedirectResponse(url="/", status_code=302)
 
     user = await AdminRepository.find_by_id(int(user_id))
+    if not user:
+        return RedirectResponse(url="/", status_code=302)
 
     return user
